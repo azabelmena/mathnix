300f478357bcac1503f9efc57f98a42d177fc571
Alec S. Zabel-Mena <alec.zabel@upr.edu>
2024-05-30 15:33:05 -0400

    Set up dev environment using the flint library for number theory.

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+
+  description = "Nix Flakes for Mathematics";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+};
+
+outputs = { self, nixpkgs }:
+let
+  system = "x86_64-linux";
+  pkgs = nixpkgs.legacyPackages.${system};
+
+in
+  {
+    packages.${system}.default = ( import ./derivations/flint2.nix { inherit pkgs ; } );
+
+    devShells.${system} = {
+      ffal = pkgs.mkShell{
+        name = "FFAL";
+
+
+        nativeBuildInputs = with pkgs; [
+          autoconf
+          openblas
+          ntl
+          automake
+          coreutils
+          gcc
+          gdb
+          gettext
+          openblas
+          ntl
+          glibc
+          gmp
+          gnumake
+          libtool
+          mpfr
+          python3
+          sage
+        ];
+
+        flint = pkgs.callPackage ./derivations/flint.nix {};
+
+        NIX_CFLAGS_COMPILE = ''
+          -O3
+          -lgmp
+          -lmpfr
+          -I${pkgs.callPackage ./derivations/flint.nix {}}/include/
+          -L${pkgs.callPackage ./derivations/flint.nix {}}/lib/
+        '';
+      };
+    };
+
+  };
+}
